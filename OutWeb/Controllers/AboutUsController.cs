53cbfb36bccf47658933c6500039eafab994d55f
Gary Chang <gary661116@gmail.com>
2018-02-11 11:18:01 +0800

    前端頁面更新

--- OutWeb/Controllers/AboutUsController.cs
+++ OutWeb/Controllers/AboutUsController.cs
@@ -1,13 +1,36 @@
-﻿using OutWeb.Enums;
-using OutWeb.Service;
+﻿using System.Linq;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using OutWeb.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+/*Json.NET相關的命名空間*/
+using Newtonsoft.Json;
 
 namespace OutWeb.Controllers
 {
     public class AboutUsController : Controller
     {
+        //== Class 建立 =========================================//
+        DBService DB = new DBService();
+        //News CNews = new News();
+        Language Clang = new Language();
+        AboutUs CAboutUs = new AboutUs();
+        //JoinUs CJoinUs = new JoinUs();
+        //Edu CEdu = new Edu();
+        //Focus CFocus = new Focus();
+        //=== 變數設定  =========================================//
+        String Img_Path = "~/Images";
+        //=== Log 記錄 =========================================//
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        //=====================================================//
+
         // GET: AboutUs
         public AboutUsController()
         {
@@ -16,30 +39,67 @@ namespace OutWeb.Controllers
 
         public ActionResult Index()
         {
+
             return RedirectToAction("TCG");
         }
 
-        // 什麼是台灣民政府
+        // 台灣民政府由來
         public ActionResult TCG()
         {
+            //抓取資料
+            string cate_id = "1";
+            DataTable dt;
+            string err_msg = "";
+            string lang_id = "zh-tw";
+            dt = CAboutUs.List(ref err_msg, "", "sort desc", "Y", "", cate_id, lang_id);
+
+            ViewData["dt"] = dt;
             return View();
         }
 
         // 關於我們 - 主張與立場
         public ActionResult Position()
         {
+            //抓取資料
+            string cate_id = "2";
+            DataTable dt;
+            string err_msg = "";
+            string lang_id = "zh-tw";
+            dt = CAboutUs.List(ref err_msg, "", "sort desc", "Y", "", cate_id, lang_id);
+
+            ViewData["dt"] = dt;
             return View();
         }
 
         // 關於我們 - 台灣地位的聲明
         public ActionResult Statement()
         {
+            //抓取資料
+            string cate_id = "3";
+            DataTable dt;
+            string err_msg = "";
+            string lang_id = "zh-tw";
+            dt = CAboutUs.List(ref err_msg, "", "sort desc", "Y", "", cate_id, lang_id);
+
+            ViewData["dt"] = dt;
             return View();
         }
 
         // 關於我們 - 法律依據
         public ActionResult Law()
         {
+            //抓取資料
+            string cate_id = "4,5,6";
+            DataTable dt;
+            DataTable d_cate;
+            string err_msg = "";
+            string lang_id = "zh-tw";
+            d_cate = CAboutUs.Cate_List(ref err_msg, cate_id, "sort", "Y", "", lang_id);
+            dt = CAboutUs.List(ref err_msg, "", "sort desc", "Y", "", cate_id, lang_id);
+
+            ViewData["d_cate"] = d_cate;
+            ViewData["dt"] = dt;
+
             return View();
         }
 
