fa01be68ffef1499f88cf42f74d5370138052472
JACKY <j205073@gmail.com>
2018-03-19 22:24:48 +0800

    補檔

---
+++ OutWeb/Models/FrontModels/News/EventStatesModels/EventStatesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutWeb.Models.FrontModels.News.EventStatesModels
+{
+    public class EventStatesViewModel
+    {
+        EventStatesListFilter m_filter = new EventStatesListFilter();
+        EventStatesResult m_result = new EventStatesResult();
+
+        public EventStatesListFilter Filter { get { return this.m_filter; } set { this.m_filter = value; } }
+        public EventStatesResult Result { get { return this.m_result; } set { this.m_result = value; } }
+    }
+}
