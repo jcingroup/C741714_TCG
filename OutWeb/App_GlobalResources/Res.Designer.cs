8219c1de6402677f1b78a1e02f6e8bb1214b79e0
Tin <yyuuix@gmail.com>
2020-06-10 18:01:32 +0800

    首頁改版+內頁拿掉咖啡色擴充

--- OutWeb/App_GlobalResources/Res.Designer.cs
+++ OutWeb/App_GlobalResources/Res.Designer.cs
@@ -919,7 +919,7 @@ namespace OutWeb.App_GlobalResources {
         }
         
         /// <summary>
-        ///   查詢類似 (07) 323-1890 的當地語系化字串。
+        ///   查詢類似 (07) 787-6459 的當地語系化字串。
         /// </summary>
         public static string tel {
             get {
